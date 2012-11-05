9aee0fd6857cee65702ad32e5a9b918d69985a34
Chyld Medford <chyld.medford@gmail.com>
2012-11-05 13:59:02 -0500

    all calculators

--- labs/basic_calculator_v3.rb
+++ labs/basic_calculator_v3.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-puts "Do you want to (a)dd, (s)ubtract, (m)ultipy, (d)ivide or (q)uit?"
+puts "Do you want to (a)dd, (s)ubtract, (m)ultipy, (d)ivide, (p)ower, sq(u)are root or (q)uit?"
 response = gets.chomp
 
 while response != 'q'
@@ -17,9 +17,13 @@ while response != 'q'
     total = first * second
   elsif response == 'd'
     total = first / second
+  elsif response == 'p'
+    total = first ** second
+  elsif response == 'u'
+    total = Math.sqrt(first)
   end
 
   puts "The result of #{first} and #{second} is #{total}"
-  puts "Do you want to (a)dd, (s)ubtract, (m)ultipy, (d)ivide or (q)uit?"
+  puts "Do you want to (a)dd, (s)ubtract, (m)ultipy, (d)ivide, (p)ower, s(q)uare root or (q)uit?"
   response = gets.chomp
 end
