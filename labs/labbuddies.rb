9b815f39a53d3c02e37af7cd494ae8f1185c8799
Chyld Medford <chyld.medford@gmail.com>
2012-11-09 09:26:14 -0500

    wired

---
+++ labs/labbuddies.rb
@@ -0,0 +1,15 @@
+require 'pry'
+require 'active_support/all'
+
+students = ['Adrian Bautista', 'Larry Buchanan', 'Jasmine Chabra', 'Raymond Chan', 'Dustin Coates', 'Aaron Fuchs', 'Chris Goodmacher', 'Zahra Jabini', 'Avinash Karnani', 'Gaurav Karnani', 'Audric Kim', 'Jonathan Ku', 'Derrick Lannaman', 'Tim LaTorre', 'Hsi-Chang Lin', 'Marc Whitman', 'Thomas Yang']
+puts `clear`
+puts "Group size?"
+size = gets.to_f
+is_lonely = (students.count % size.to_i) == 1
+
+if is_lonely
+  ((students.count / size) - 1).truncate.times{|x| puts "Group #{x}: #{students.pop(size).to_sentence}"}
+  puts "Group z: #{students.to_sentence}"
+else
+  (students.count / size).ceil.times{|x| puts "Group #{x}: #{students.pop(size).to_sentence}"}
+end
---
+++ labs/wired.rb
@@ -0,0 +1,28 @@
+require 'pry'
+require 'active_support/all'
+
+class Wire
+  attr_accessor :points, :length
+  def initialize
+    @points = 10.times.map { Point.new }
+  end
+  def distance(p1, p2)
+    Math.sqrt(((p1.x - p2.x)**2) + ((p1.y - p2.y)**2))
+  end
+  def length
+    (points.length - 1).times.map {|x| distance(points[x], points[x+1])}.inject(&:+)
+  end
+  def to_s
+    "This wire is of length #{length}"
+  end
+end
+
+class Point
+  attr_accessor :x, :y
+  def initialize
+    @x = Random.rand(1000)
+    @y = Random.rand(1000)
+  end
+end
+
+puts Wire.new
